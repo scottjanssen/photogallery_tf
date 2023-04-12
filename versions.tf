1248aeeb845ed2785ffc57ec6a5173210ba79d14
Scott Janssen <scottjanssen@lawn-143-215-124-32.lawn.gatech.edu>
2023-04-12 19:47:21 -0400

    Changes in versions

--- versions.tf
+++ versions.tf
@@ -1,4 +1,11 @@
 
 terraform {
   required_version = ">= 0.12"
+  backend "s3" {
+    bucket = "photobucket-janssen-scott-cs4803"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+    access_key = var.AWS_ACCESS_KEY
+    secret_key = var.AWS_SECRET_KEY
+  }
 }
